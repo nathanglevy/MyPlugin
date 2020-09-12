8a70281b7e5f4b2d5d51da3f6b32742ff35344a1
GitHub Action <action@github.com>
2020-09-12 20:14:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/nathanglevy/myplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/nathanglevy/myplugin/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nathanglevy.myplugin.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nathanglevy.myplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/nathanglevy/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nathanglevy/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nathanglevy.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nathanglevy.myplugin.MyBundle
 
 class MyApplicationService {
 
